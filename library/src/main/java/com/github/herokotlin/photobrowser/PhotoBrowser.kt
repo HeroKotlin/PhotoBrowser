a65b9092662abcf7fa3cb3931e11b08a097e1122
musicode <musicode@qq.com>
2019-11-06 11:04:45 +0800

    bump 0.4.4
    androidx

--- library/src/main/java/com/github/herokotlin/photobrowser/PhotoBrowser.kt
+++ library/src/main/java/com/github/herokotlin/photobrowser/PhotoBrowser.kt
@@ -1,8 +1,8 @@
 package com.github.herokotlin.photobrowser
 
 import android.content.Context
-import android.support.v4.view.PagerAdapter
-import android.support.v4.view.ViewPager
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.PagerAdapter
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
